77cc223bb6148b37e782d26e49f9e64dd73240f0
ShubhamUpad <01shubhamupadhyay@gmail.com>
2022-10-23 00:05:15 +0530

    first commit

---
+++ basecode.py
@@ -0,0 +1,15 @@
+import datetime as dt
+import requests
+
+baseUrl = "https://api.openweathermap.org/data/2.5/weather?"
+apiKey = "appid=f248ae5e389d6870cc0b7dd267b92555"
+city = "Delhi"
+
+url = baseUrl + apiKey + "&q=" + city
+
+response = requests.get(url).json()
+
+print(response)
+
+print(str(response['main']['temp'] - 273.15)[0:5])
+print(response['weather'][0]['id'])
