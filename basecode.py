e6b2108ef425fb317ad81ece8e1d4f6392451aa5
ShubhamUpad <01shubhamupadhyay@gmail.com>
2022-10-23 12:35:55 +0530

    API key.txt

--- basecode.py
+++ basecode.py
@@ -2,14 +2,17 @@ import datetime as dt
 import requests
 
 baseUrl = "https://api.openweathermap.org/data/2.5/weather?"
-apiKey = "appid=f248ae5e389d6870cc0b7dd267b92555"
+apikey = open('apikey.txt', 'r').read()
+
+print(type(apikey))
 city = "Delhi"
 
-url = baseUrl + apiKey + "&q=" + city
+url = baseUrl + "appid=" + apikey + "&q=" + city
 
 response = requests.get(url).json()
 
-# print(response)
-
+print(response)
+'''
 print(str(response['main']['temp'] - 273.15)[0:5])
 print(response['weather'][0]['description'])
+'''
